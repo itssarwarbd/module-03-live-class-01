d9134ed27bc647837fd2f7bcc4aa5566644331c7
Sarwar Husain <sarwar.info2000@gmail.com>
2024-03-18 04:36:00 +0600

    Upload JavaScript Code

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+let n1 = 16;
+let n2 = 4;
+let sum = n1 + n2;
+
+// console.log(sum);
+// let course = "Mastering "; //String
+// let duration = 16;  //Number - Integer
+// let courseRunning = true; //Boolean
+
+// console.log(course);
+// console.log(duration);
+// console.log(courseRunning);
